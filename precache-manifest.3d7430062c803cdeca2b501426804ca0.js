d21d7ced7f9a02527f6bef93ea8135579634bcfa
VrajKD <vrajkishan.dugar2017@vitstudent.ac.in>
2019-12-07 13:13:16 +0530

    Updates

---
+++ precache-manifest.3d7430062c803cdeca2b501426804ca0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95e01c873e419c6cd58e14cf216cda5f",
+    "url": "/vkd-kart/index.html"
+  },
+  {
+    "revision": "4d229616a4e0af8d35c1",
+    "url": "/vkd-kart/static/css/main.b2c89fdd.chunk.css"
+  },
+  {
+    "revision": "df50a09be52e5302273a",
+    "url": "/vkd-kart/static/js/2.24255851.chunk.js"
+  },
+  {
+    "revision": "4d229616a4e0af8d35c1",
+    "url": "/vkd-kart/static/js/main.bc49145d.chunk.js"
+  },
+  {
+    "revision": "cbc7d2365ceb6529d4f1",
+    "url": "/vkd-kart/static/js/runtime-main.3c6fce3c.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/vkd-kart/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/vkd-kart/static/media/shopping-bag.30f07071.svg"
+  }
+]);
