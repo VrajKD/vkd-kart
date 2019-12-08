683820c803112d8e13f2c6e2f08b9498ec50b37a
VrajKD <vrajkishan.dugar2017@vitstudent.ac.in>
2019-12-09 00:27:46 +0530

    Local session storage using Redux-persist for items in cart, prevents from emptying cart on refresh

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 
 import logger from 'redux-logger'; //Middleware
 
@@ -7,9 +8,9 @@ import rootReducer from './root-reducer';
 //SETUP MIDDLEWARES
 
 const middlewares = [logger]; //Because we willl use many middlewares and now we can pass it in directlyand as react is expectng array as middleware
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
-
-export default store;
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export  const persistor = persistStore(store);
+export default { store, persistor };
 
 
 
