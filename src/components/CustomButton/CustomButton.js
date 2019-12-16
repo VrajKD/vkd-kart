68682186a3b9112e92554091d5dd370c0df23cae
VrajKD <vrajkishan.dugar2017@vitstudent.ac.in>
2019-12-16 17:14:11 +0530

    Removed unwanted console.logs

--- src/components/CustomButton/CustomButton.js
+++ src/components/CustomButton/CustomButton.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './CustomButton.scss'
 
 const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherButtonProps }) => {
-    console.log(inverted);
     return (
         <button className={`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-signin' : ''} custom-button`} {...otherButtonProps}>
             {children}
--- src/pages/CollectionPage/CollectionPage.js
+++ src/pages/CollectionPage/CollectionPage.js
@@ -9,16 +9,15 @@ class CollectionPage extends React.Component {
     render() {
         const { collection } = this.props;
         const { title, items } = collection;
-        console.log(collection)
         return (
             <div className="collection-page">
                 <div className="title">{title}</div>
                 <div className="items">
-                {
-                    items.map(item => (
-                        <CollectionItem id={item.id} item={item} />
-                    ))
-                }
+                    {
+                        items.map(item => (
+                            <CollectionItem id={item.id} item={item} />
+                        ))
+                    }
                 </div>
             </div>)
     }
